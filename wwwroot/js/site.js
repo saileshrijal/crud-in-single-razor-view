b38c0cdae964be45765aa4e906e77d42da718a00
saileshrijal <saileshrijal1@gmail.com>
2022-07-13 09:41:12 +0545

    final commit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,15 +3,3 @@
 
 // Write your JavaScript code.
 
-ShowInPopup = (url, title) => {
-    $.ajax({
-        type: "GET",
-        url: url,
-        success: function (res) {
-            $('#form-modal .modal-body').html(res)
-            $('#form-modal .modal-title').html(title)
-            $('#form-modal').modal('show');
-        }
-    })
-
-}
